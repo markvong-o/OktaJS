081f252f2f388012361f1faced3bfe7f162e92ab
markvong-o <83755325+markvong-o@users.noreply.github.com>
2022-06-27 12:44:39 -0700

    First commit

---
+++ 0oahn1tdxWkf9P366696.js
@@ -0,0 +1,16 @@
+function getClientId() {
+  if (!OktaUtil) return undefined;
+  var requestContext = OktaUtil.getRequestContext();
+  if (requestContext && requestContext.app && requestContext.app.value.id) {
+    return requestContext.app.value.id;
+  }
+}
+
+var head = document.head;
+var link = document.createElement('link');
+link.type = 'text/css';
+link.rel = 'stylesheet';
+link.href = 'https://markvong-o.github.io/OktaCSS/' + getClientId() + '.css';
+head.appendChild(link);
+
+console.log(getClientId(), "testing this...");
